e9aa4f0126633440cd353c1c363edc1a3482c053
aajunwa <45833272+aajunwa@users.noreply.github.com>
2023-09-22 21:12:46 -0600

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,8 @@
+﻿public class Node
+{
+     
+    public Node LeftNode { get; set; }
+    public Node RightNode { get; set; }
+    public int Data { get; set; }
+     
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Binary_Tree b = new Binary_Tree();
+b.Add(15);
+b.Add(12);
+b.Add(20);
+b.Add(11);
+b.Add(21);
+//b.Add(13);
+Console.WriteLine(b.GetTreeDepth());
+//b.Remove(12);
+b.TraverseInOrder(b.Root);
+Console.WriteLine();
+/*Node search = b.Find(15);
+try
+{
+    if (search.Data != null)
+    Console.WriteLine("Data was found");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Data doesnt exist");
+}*/
